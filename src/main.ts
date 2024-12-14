d96c79264ca3f2a8e9a45407d3234b3f9190ba3e
iman <imsofos@gmail.com>
2024-12-15 03:15:08 +0330

    swagger

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,24 @@ import { CASLFilter } from './common/filters/casl.filter';
 import { PrismaFilter } from './common/filters/prisma.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new JWTFilter(), new CASLFilter(), new PrismaFilter());
-  app.useStaticAssets(join(__dirname, '..', 'public'))
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {prefix: '/uploads/'})
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {prefix: '/uploads/'});
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
